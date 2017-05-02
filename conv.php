27c13edae79a9e70615b6fd1573747b31106e9f6
robkop <robert.kopydlowski@gmail.com>
2017-05-02 22:28:29 +0200

    XML logic for Hints and new menu options

---
+++ conv.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Triathlon Calculator - VO2MAX</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+  <?php $page = 'conv'?>
+</head>
+
+<body>
+  <?php include 'elements/head.php'; ?>
+  <div class="container">
+    <?php include 'elements/nav.php'; ?>
+    <div class="row">
+      <div class="jumbotron">
+        <h2>Check your V0<sup>2</sup> MAX index</h2>
+        <p>Calculate based on Cooper Test (full 12 minutes long test) result.</p>
+        <div class="well well-lg">
+          <form>
+            <div class="radio">
+              <label>
+                <input type="radio" name="optionsRadios" id="opt1" value="option1" checked>
+                Speed = > Pace
+              </label>
+            </div>
+            <div class="radio">
+              <label>
+                <input type="radio" name="optionsRadios" id="opt1" value="option2">
+                Pace = > Speed
+              </label>
+            </div>
+          </form>
+          <button type="button" class="btn btn-success" data-toggle="collapse" data-target="#finishrow" id="showbtn">Show result</button>
+        </div>
+      </div>
+    <div class="row" id="call">
+      <div class="col-sm-12 text-center" style="display:none" id="finishrow">
+        <h1>Your VO<sup>2</sup> MAX is:</h1>
+        <h1 id="result"></h1>
+      </div>
+    </div>
+    <?php include 'elements/footer.php'; ?>
+  </div>
+</div>
+  <script src="scripts/oxy.js"></script>
+</body>
+
+</html>
--- elements/nav.php
+++ elements/nav.php
@@ -4,6 +4,9 @@
       <li <? if($page == 'home'): ?> class="active"<? endif ?>><a href="index.php">Home</a></li>
       <li<? if($page == 'pace'): ?> class="active"<? endif ?>><a href="pace.php">TriCalc with pace</a></li>
       <li <? if($page == 'vo2max'): ?> class="active"<? endif ?>><a href="oxy.php">V0<sup>2</sup> MAX</a></li>
+      <li <? if($page == 'conv'): ?>< class="active"<? endif ?>><a href='conv.php'>Pace/Speed converter</a></li>
+      <li <? if($page == 'hints'): ?> class="active"<? endif ?>><a href="hints.php">TriHints</a></li>
+      <li <? if($page == 'checklist'): ?> class="active"<? endif ?>><a href="checklist.php">Race Day checklist</a></li>
     </ul>
   </div>
 </nav>
