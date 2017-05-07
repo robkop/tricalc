9aa96315f2656437dd2fc07d4a3282c0dbe80da9
robkop <robert.kopydlowski@gmail.com>
2017-05-07 08:12:42 +0200

    converter logic

--- conv.php
+++ conv.php
@@ -18,36 +18,56 @@
     <?php include 'elements/nav.php'; ?>
     <div class="row">
       <div class="jumbotron">
-        <h2>Check your V0<sup>2</sup> MAX index</h2>
-        <p>Calculate based on Cooper Test (full 12 minutes long test) result.</p>
         <div class="well well-lg">
           <form>
             <div class="radio">
               <label>
-                <input type="radio" name="optionsRadios" id="opt1" value="option1" checked>
+                <input type="radio" name="optionsRadios" id="opt1" value="option1">
                 Speed = > Pace
               </label>
             </div>
             <div class="radio">
               <label>
-                <input type="radio" name="optionsRadios" id="opt1" value="option2">
+                <input type="radio" name="optionsRadios" id="opt2" value="option2">
                 Pace = > Speed
               </label>
             </div>
           </form>
-          <button type="button" class="btn btn-success" data-toggle="collapse" data-target="#finishrow" id="showbtn">Show result</button>
+          <button type="button" class="btn btn-success" data-toggle="collapse" data-target="#txc" id="showbtn">Show result</button>
+        </div>
+        <div class="well well-lg" id='txc' style="display:none">
+          <form>
+            <div class="form-group">
+              <label for="speed">Speed [km/h]</label>
+              <input type="number" class="form-control dsb3" id="speed" min="0">
+            </div>
+            <button type="button" class="btn btn-success" id="2next">OK</button>
+          </form>
+        </div>
+        <div class="well well-lg" id='txd' style="display:none">
+          <form>
+            <div class="form-group">
+              <label for="pace">Pace [min/km]</label>
+              <input type="text" class="form-control dsb3" id="pace" min="0">
+            </div>
+            <button type="button" class="btn btn-success" id="3next">OK</button>
+          </form>
         </div>
       </div>
-    <div class="row" id="call">
-      <div class="col-sm-12 text-center" style="display:none" id="finishrow">
-        <h1>Your VO<sup>2</sup> MAX is:</h1>
+    <div class="row" id="call1">
+      <div class="col-sm-12 text-center" style="display:none" id="paceToSpeed">
         <h1 id="result"></h1>
+      </div>
+      <div class="row" id="call2">
+        <div class="col-sm-12 text-center" style="display:none" id="speedToPace">
+          <h1 id="result2"></h1>
+        </div>
       </div>
     </div>
     <?php include 'elements/footer.php'; ?>
   </div>
 </div>
-  <script src="scripts/oxy.js"></script>
+  <script src="scripts/converter.js"></script>
 </body>
 
 </html>
--- me.php
+++ me.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>Triathlon Calculator</title>
+  <title>Me</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -10,27 +10,17 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+  <?php $page = 'pace'?>
 </head>
 
 <body>
 
-  <div class="jumbotron text-center mg j1" id="j1">
-    <h1> Robert Kopydłowski </h1>
+  <div class="container fluid">
+    <div class="row">
+      <div class="col-md-12" id="first">
+        sdfsdfsdfsd
+      </div>
+
   </div>
-  <div class="jumbotron text-center j4 mg" id="j4">
-    <h1> Blog </h1>
-  </div>
-  <div class="jumbotron text-center mg j2" id="j2">
-    <h1> Sport results </h1>
-    <h2>enduhub.com/en/robkop/</h2>
-  </div>
-  <div class="jumbotron text-center mg j3" id="j3">
-    <h1> GitHub </h1>
-    <h2>github.com/robkop</h2>
-  </div>
-  <div class="jumbotron text-center mg j5" id="j5">
-      <h1> Back to the studio </h1>
-  </div>
-<script <script src="scripts/me.js"></script>
 </body>
 </html>
